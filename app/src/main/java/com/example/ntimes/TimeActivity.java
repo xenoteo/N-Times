a96b600a3ff1d3f408b93977605909a043a0bf37
xenofiodor <fedorovxa@gmail.com>
2020-08-16 18:20:11 +0200

    Running timers one after another handled

--- app/src/main/java/com/example/ntimes/TimeActivity.java
+++ app/src/main/java/com/example/ntimes/TimeActivity.java
@@ -12,21 +12,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TimeActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
 
     private TextView roundTextView;
+    private TextView roundDisplay;
+
     private TextView exerciseTextView;
+    private TextView exerciseDisplay;
+
     private TextView remainingDisplay;
     private TextView remainingTextView;
+
     private Button timeButton;
+
+    private int exerciseNumber; // for the same rounds
+
 
     private CountDownTimer currentCountDown;
     private long remain;
-    private boolean isExercise;
+    private final List<Integer> times = new ArrayList<>();
 
-    private MediaPlayer startExercise;
-    private MediaPlayer stopExercise;
+    private MediaPlayer startExerciseMusic;
+    private MediaPlayer stopExerciseMusic;
+
+    private boolean isStarted = false;
+//    Runn boolean isFinished;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +50,6 @@ public class TimeActivity extends AppCompatActivity {
         setSharedPreferences();
         setViews();
         setMusic();
-        startTimer();
     }
 
     private void setSharedPreferences(){
@@ -44,71 +57,106 @@ public class TimeActivity extends AppCompatActivity {
     }
 
     private void setViews(){
-        roundTextView = (TextView)findViewById(R.id.roundDisplay);
-        exerciseTextView = (TextView)findViewById(R.id.exerciseDisplay);
-        remainingDisplay = (TextView)findViewById(R.id.remainingDisplay);
-        remainingTextView = (TextView)findViewById(R.id.remaining);
-        timeButton = (Button)findViewById(R.id.timerButton);
+        roundTextView = findViewById(R.id.textRound);
+        roundDisplay = findViewById(R.id.roundDisplay);
+        exerciseTextView = findViewById(R.id.textExercise);
+        exerciseDisplay = findViewById(R.id.exerciseDisplay);
+        remainingDisplay = findViewById(R.id.remainingDisplay);
+        remainingTextView = findViewById(R.id.remaining);
+        timeButton = findViewById(R.id.timerButton);
     }
 
     private void setMusic(){
-        startExercise = MediaPlayer.create(this, R.raw.start);
-        stopExercise = MediaPlayer.create(this, R.raw.stop);
+        startExerciseMusic = MediaPlayer.create(this, R.raw.start);
+        stopExerciseMusic = MediaPlayer.create(this, R.raw.stop);
     }
 
-    private void startTimer(){
+    private void setRunningTimers(){
         boolean roundsSame = sharedPreferences.getBoolean(Key.ROUNDS_SAME, true);
-        if (roundsSame) makeTimerWithSameRounds();
-        else makeTimerWithDifferentRounds();
+        if (roundsSame) runWithSameRounds();
+        else runWithDifferentRounds();
     }
 
-    private void makeTimerWithSameRounds(){
-        currentCountDown = makeTimer(10000);
-        isExercise = true;
-        /*
+    private void runWithSameRounds() {
+        exerciseNumber = sharedPreferences.getInt(Key.EXERCISES_NUMBER, 0);
         int roundsNumber = sharedPreferences.getInt(Key.ROUNDS_NUMBER, 0);
-        int exerciseNumber = sharedPreferences.getInt(Key.EXERCISES_NUMBER, 0);
         int exerciseTime = sharedPreferences.getInt(Key.EXERCISE_TIME, 0) *  1000;
-        int exerciseRestTime = sharedPreferences.getInt(Key.EXERCISE_TIME, 0) *  1000;
-        int roundRestTime = sharedPreferences.getInt(Key.EXERCISE_TIME, 0) *  1000;
+        int exerciseRestTime = sharedPreferences.getInt(Key.EXERCISES_REST_TIME, 0) *  1000;
+        int roundRestTime = sharedPreferences.getInt(Key.ROUNDS_REST_TIME, 0) *  1000;
+
+
         for (int i = 1; i <= roundsNumber; i++){
-            roundTextView.setText(String.valueOf(i));
-            for (int j = 1; j <= exerciseNumber; j++){
-                exerciseTextView.setText(String.valueOf(j));
-                isExercise = true;
-                currentCountDown = makeTimer(exerciseTime).start();
-                isExercise = false;
-                currentCountDown = makeTimer(exerciseRestTime).start();
+            roundDisplay.setText(String.valueOf(i));
+            for (int j = 1; j < exerciseNumber; j++){
+                times.add(exerciseTime);
+                times.add(exerciseRestTime);
             }
-            currentCountDown = makeTimer(roundRestTime).start();
+            times.add(exerciseTime);
+            times.add(roundRestTime);
         }
-         */
+
+        times.remove(times.size() - 1);
+        startTimer(0);
     }
 
-    private void makeTimerWithDifferentRounds(){
+    private void startTimer(int id){
+        if ((id + 1) % (2 * exerciseNumber) == 0) { // if round rest
+            setRoundVisibility(View.INVISIBLE);
+        }
+        else{
+            setRoundVisibility(View.VISIBLE);
+            roundDisplay.setText(String.valueOf((id + 1) / (2 * exerciseNumber) + 1));
+            if (id % 2 == 0) {  // if exercise
+                setExerciseVisibility(View.VISIBLE);
+                exerciseDisplay.setText(String.valueOf((id % (2 * exerciseNumber)) / 2 + 1));
+            }
+            else
+                setExerciseVisibility(View.INVISIBLE);
+        }
+        makeTimer(times.get(id), id).start();
+    }
+
+    private void runWithDifferentRounds(){
         // TODO
     }
 
-    private CountDownTimer makeTimer(int millisInFuture){
+    private void setRoundVisibility(int visibility){
+        roundTextView.setVisibility(visibility);
+        roundDisplay.setVisibility(visibility);
+        exerciseTextView.setVisibility(visibility);
+        exerciseDisplay.setVisibility(visibility);
+    }
+
+    private void setExerciseVisibility(int visibility){
+        exerciseTextView.setVisibility(visibility);
+        exerciseDisplay.setVisibility(visibility);
+    }
+
+    private CountDownTimer makeTimer(int millisInFuture, int id){
         return new CountDownTimer(millisInFuture, 1000) {
             @Override
             public void onTick(long l) {
-                if (isExercise){
+                if (id % 2 == 0) // if exercise
                     remainingTextView.setText(R.string.exercise_remain);
-                }
-                else {
+                else
                     remainingTextView.setText(R.string.rest_remain);
-                }
+
                 remainingDisplay.setText(String.valueOf(l / 1000));
                 remain = l;
             }
             @Override
             public void onFinish() {
-                if (isExercise) stopExercise.start();
-                else startExercise.start();
+                if (id % 2 == 0)  // if exercise
+                    stopExerciseMusic.start();
+                else
+                    startExerciseMusic.start();
+                startTimer(id + 1);
             }
         };
     }
+
+
+
 
     public void handleTimeButtonClick(View v){
         String pauseColor = "#FF5722";
@@ -116,7 +164,18 @@ public class TimeActivity extends AppCompatActivity {
 
         String value = timeButton.getText().toString();
 
-        if (value.equals(getString(R.string.start)) || value.equals(getString(R.string.resume))){
+        if (!isStarted) {
+            isStarted = true;
+            timeButton.setText(R.string.pause);
+            timeButton.setBackgroundColor(Color.parseColor(pauseColor));
+            setRunningTimers();
+        }
+        /*
+        else if (isFinished) {
+            // todo
+        }
+
+        else if (value.equals(getString(R.string.resume))){
             timeButton.setText(R.string.pause);
             timeButton.setBackgroundColor(Color.parseColor(pauseColor));
             currentCountDown.start();
@@ -127,7 +186,8 @@ public class TimeActivity extends AppCompatActivity {
             currentCountDown.cancel();
             currentCountDown = makeTimer((int) remain);
         }
-    }
 
+         */
+    }
 
 }
